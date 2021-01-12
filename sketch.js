512a54fb44f09198af537153e3a76afa5e6f087f
XaNdEr <70212030+XaNdEr2579@users.noreply.github.com>
2021-01-12 16:51:21 +0530

    commit done

---
+++ sketch.js
@@ -0,0 +1,13 @@
+var player;
+
+function setup() {
+    //This runs once
+    createCanvas(400,400);
+    player = createSprite (200,200,10,10)
+    
+}
+
+function draw() {
+    //This runs again and again
+drawSprites();
+}
